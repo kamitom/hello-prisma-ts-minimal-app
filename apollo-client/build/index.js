e8429778c9f8d71363b318d3c996b8a36d775157
Tom Chen <tom@itri.org.tw>
2020-03-01 14:39:11 +0800

    feat: add apollo-client

---
+++ apollo-client/build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const faker_1 = __importDefault(require("faker"));
+const fName = faker_1.default.name.findName();
+console.log(`hello, ts-node-v5, ${fName}`);
---
+++ apollo-client/src/index.ts
@@ -0,0 +1,5 @@
+import faker from 'faker';
+
+const fName = faker.name.findName();
+
+console.log(`hello, ts-node-v5, ${fName}`);
